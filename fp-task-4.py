fbfcadc07d5c47ab4fa5d871b33782c65e8ad911
OlenaBogoliubova <143827030+OlenaBogoliubova@users.noreply.github.com>
2024-03-12 20:30:50 +0100

    Update fp-task-4.py
    Додавання функції побудови бінарної купи без дублювання коду

--- fp-task-4.py
+++ fp-task-4.py
@@ -69,56 +69,22 @@ root.right.left = Node(3)
 # Відображення дерева
 draw_tree(root)
 
-
-class MaxHeapNode:
-    def __init__(self, key, color="skyblue"):
-        self.left = None
-        self.right = None
-        self.val = key
-        self.color = color
-        self.id = str(uuid.uuid4())
-
-
-def add_heap_edges(graph, node, pos, x=0, y=0, layer=1):
-    if node is not None:
-        graph.add_node(node.id, color=node.color, label=node.val)
-        if node.left:
-            graph.add_edge(node.id, node.left.id)
-            l = x - 1 / 2 ** layer
-            pos[node.left.id] = (l, y - 1)
-            l = add_heap_edges(graph, node.left, pos, x=l,
-                               y=y - 1, layer=layer + 1)
-        if node.right:
-            graph.add_edge(node.id, node.right.id)
-            r = x + 1 / 2 ** layer
-            pos[node.right.id] = (r, y - 1)
-            r = add_heap_edges(graph, node.right, pos, x=r,
-                               y=y - 1, layer=layer + 1)
-    return graph
+def build_heap_tree(heap_array):
+    def build_heap_tree_helper(index):
+        if index < len(heap_array):
+            node = Node(heap_array[index])
+            node.left = build_heap_tree_helper(2 * index + 1)
+            node.right = build_heap_tree_helper(2 * index + 2)
+            return node
+        return None
+    
+    return build_heap_tree_helper(0)
+# Бінарна купа у вигляді масиву
+heap_array = [1, 3, 5, 7, 9, 2, 4, 34, 2, 1, 2]
+heapq.heapify(heap_array)
+# Побудова дерева з купи
+heap_tree_root = build_heap_tree(heap_array)
+# Відображення бінарної купи у вигляді дерева
+draw_tree(heap_tree_root)
 
 
-def draw_heap(heap_root):
-    heap = nx.DiGraph()
-    pos = {heap_root.id: (0, 0)}
-    heap = add_heap_edges(heap, heap_root, pos)
-
-    colors = [node[1]['color'] for node in heap.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in heap.nodes(data=True)}
-
-    plt.figure(figsize=(8, 5))
-    nx.draw(heap, pos=pos, labels=labels, arrows=False,
-            node_size=2500, node_color=colors)
-    plt.show()
-
-
-# Створення бінарної купи
-heap_root = MaxHeapNode(15)
-heap_root.left = MaxHeapNode(13)
-heap_root.right = MaxHeapNode(12)
-heap_root.left.left = MaxHeapNode(8)
-heap_root.left.right = MaxHeapNode(5)
-heap_root.right.left = MaxHeapNode(3)
-heap_root.right.right = MaxHeapNode(10)
-
-# Відображення бінарної купи
-draw_heap(heap_root)
